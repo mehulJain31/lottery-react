7924c6306c62d9941908d423346c9b6dd8991cec
Mehul Jain <mehuljain@Mehuls-MBP.attlocal.net>
2022-08-31 22:33:49 -0500

    Added web3 abi and address to the app.js and retrieved the manager of the lottery contract

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import web3 from './web3';
+import web3 from "./web3";
+import lottery from "./lottery";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { manager: "" };
+  }
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+
+    this.setState({ manager });
+  }
   render() {
-    web3.eth.getAccounts().then(console.log);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h2>Lottery Contract</h2>
+          <h3>
+            This Contract for a lottery is managed by {this.state.manager}
+          </h3>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/mxj9017/lottery-react"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Project Github Link
           </a>
         </header>
       </div>
